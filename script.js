19618aefe162033da9d3cf878608d4bff37cf484
Glitch (hello-webpage) <none>
2021-03-14 15:11:14 +0000

    🖐🏙 Checkpoint ./script.js:5313652/3985

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const unitCount = 5;
 const gridSize = spaceSize / unitCount;
 
 const data = Array.from({ length: unitCount }, () =>
-  Array.from({ length: unitCount })
+  Array.from({ length: unitCount }).fill(0)
 );
 
-data[2][2] = 1
+data[1][1] = .5;
+data[1][2] = 1;
+data[1][3] = .5;
+data[2][1] = 1;
+data[2][2] = 1;
+data[2][3] = 1;
+data[3][1] = .5;
+data[3][2] = 1;
+data[3][3] = .5;
 
 (() => {
   const c = document.getElementById("c");
@@ -18,23 +26,22 @@ data[2][2] = 1
   ctx.imageSmoothingEnabled = false;
 
   ctx.translate(0.5, 0.5);
-
-  ctx.fillStyle = "#fff";
-  ctx.fillRect(0, 0, c.width, c.height);
-
-  ctx.strokeStyle = "#ccc";
-  strokeGrid(ctx, gridSize);
   
-  for (let x = 0; x < data.length; x++) {
-    const dataX = data[x];
-    for (let y = 0; data[x].l) {
-      ctx.rect()
-    }
-  }
+  drawBackground(c, ctx);
+
+  drawData(ctx);
 
   ctx.fillStyle = "#f00";
   fillCircle(ctx, gridSize, gridSize, 2);
 })();
+
+function drawBackground(canvas, ctx) {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.strokeStyle = "#ccc";
+  strokeGrid(ctx, gridSize);
+}
 
 function strokeGrid(ctx, gridSize) {
   ctx.beginPath();
@@ -57,4 +64,13 @@ function strokeGrid(ctx, gridSize) {
 function fillCircle(ctx, x, y, radius) {
   ctx.ellipse(x, y, radius, radius, 0, 0, 2 * Math.PI);
   ctx.fill();
+}
+
+function drawData(ctx) {
+  for (let y = 0; y < unitCount; y++) {
+    for (let x = 0; x < unitCount; x++) {
+      ctx.fillStyle = `rgba(0, 0, 0, ${data[x][y]})`;
+      ctx.fillRect(x * gridSize, y * gridSize, gridSize, gridSize);
+    }
+  }
 }
