108970ddaa3bd31be31e78c3da23950543bb4b55
Glitch (hello-webpage) <none>
2021-03-14 12:11:34 +0000

    🍳🏂 Checkpoint ./README.md:5313652/1007 ./index.html:5313652/1393 ./script.js:5313652/1665

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+const c = document.getElementsById("c");
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+c.width = 300;
+c.height = 300;
+
+const ctx = c.getContext("2d");
+
+ctx.draw;
+
+const GRID_SIZE = 10;
+
+for (let i = 0; i < c.width; i += 10) {
+  ctx.moveTo(i, 0);
+  ctx.lineTo(c.height, 0);
+}
+
+ctx.stroke();
