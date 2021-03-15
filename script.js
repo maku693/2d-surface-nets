351015ebd180d9a66dfe044ff9f319a23aeabc1f
Glitch (hello-webpage) <none>
2021-03-15 15:15:48 +0000

    ✊🍘 Checkpoint ./script.js:5313652/7968

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ const data = Array.from({ length: unitCount }, () =>
   Array.from({ length: unitCount }).fill(0)
 );
 
+data[1][1] = 0.5;
 data[1][2] = 1;
+data[1][3] = 0.5;
 data[2][1] = 1;
 data[2][2] = 1;
 data[2][3] = 1;
+data[3][1] = 0.5;
 data[3][2] = 1;
+data[3][3] = 0.5;
 
 (() => {
   const c = document.getElementById("c");
@@ -26,7 +30,7 @@ data[3][2] = 1;
 
   drawData(ctx);
 
-  drawOutlineCulled(ctx);
+  drawOutlineCulledSmooth(ctx);
 })();
 
 function drawBackground(canvas, ctx) {
@@ -56,11 +60,10 @@ function strokeGrid(ctx, gridSize) {
 }
 
 function drawData(ctx) {
-  ctx.fillStyle = "gray";
-
   for (let y = 0; y < unitCount; y++) {
     for (let x = 0; x < unitCount; x++) {
-      if (data[x][y] < Number.EPSILON) continue;
+      if (data[x][y] === 0) continue;
+      ctx.fillStyle = `rgba(0, 0, 0, ${data[x][y]})`;
       ctx.fillRect(x * gridSize, y * gridSize, gridSize, gridSize);
     }
   }
@@ -71,7 +74,7 @@ function drawOutlineStupid(ctx) {
 
   for (let y = 0; y < unitCount; y++) {
     for (let x = 0; x < unitCount; x++) {
-      if (data[x][y] < Number.EPSILON) continue;
+      if (data[x][y] === 0) continue;
       ctx.translate(x * gridSize, y * gridSize);
 
       ctx.beginPath();
@@ -93,23 +96,23 @@ function drawOutlineCulled(ctx) {
 
   for (let y = 0; y < unitCount; y++) {
     for (let x = 0; x < unitCount; x++) {
-      if (data[x][y] < Number.EPSILON) continue;
+      if (data[x][y] === 0) continue;
       ctx.translate(x * gridSize, y * gridSize);
 
       ctx.beginPath();
-      if (y > 0 && data[x][y - 1] < Number.EPSILON) {
+      if (y > 0 && data[x][y - 1] === 0) {
         ctx.moveTo(0, 0);
         ctx.lineTo(gridSize, 0);
       }
-      if (x < unitCount - 1 && data[x + 1][y] < Number.EPSILON) {
+      if (x < unitCount - 1 && data[x + 1][y] === 0) {
         ctx.moveTo(gridSize, 0);
         ctx.lineTo(gridSize, gridSize);
       }
-      if (y < unitCount - 1 && data[x][y + 1] < Number.EPSILON) {
+      if (y < unitCount - 1 && data[x][y + 1] === 0) {
         ctx.moveTo(gridSize, gridSize);
         ctx.lineTo(0, gridSize);
       }
-      if (x > 0 && data[x - 1][y] < Number.EPSILON) {
+      if (x > 0 && data[x - 1][y] === 0) {
         ctx.moveTo(0, gridSize);
         ctx.lineTo(0, 0);
       }
@@ -121,3 +124,38 @@ function drawOutlineCulled(ctx) {
     }
   }
 }
+
+function drawOutlineCulledSmooth(ctx) {
+  ctx.strokeStyle = "red";
+
+  for (let y = 0; y < unitCount; y++) {
+    for (let x = 0; x < unitCount; x++) {
+      if (data[x][y] < 1) continue;
+      ctx.translate(x * gridSize, y * gridSize);
+
+      ctx.beginPath();
+      if (y > 0 && data[x][y - 1] < 1) {
+        ctx.moveTo(0, 0);
+        ctx.lineTo(gridSize, 0);
+      }
+      if (x < unitCount - 1 && data[x + 1][y] < 1) {
+        ctx.moveTo(gridSize, 0);
+        ctx.lineTo(gridSize, gridSize);
+      }
+      if (y < unitCount - 1 && data[x][y + 1] < 1) {
+        ctx.moveTo(gridSize, gridSize);
+        ctx.lineTo(0, gridSize);
+      }
+      if (x > 0 && data[x - 1][y] < 1) {
+        ctx.moveTo(0, gridSize);
+        ctx.lineTo(0, 0);
+      }
+      ctx.closePath();
+
+      ctx.stroke();
+
+      ctx.resetTransform();
+    }
+  }
+}
+
