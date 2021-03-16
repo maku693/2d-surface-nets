ffa325c2e89e484e478c7fed00978639b02841b0
Glitch (hello-webpage) <none>
2021-03-16 14:16:57 +0000

    🌉📈 Checkpoint ./script.js:5313652/9668

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const gridSize = 40;
 const grids = worldSize / gridSize;
 const samples = grids + 1;
 
-const data = Array.from({ length: samples }, () =>
-  new Array(samples).fill(0)
-);
+const data = Array.from({ length: samples }, () => new Array(samples).fill(0));
 
+data[2][1] = 0.5;
 data[2][2] = 1;
 data[2][3] = 1;
+data[2][4] = 0.5;
+data[3][1] = 0.5;
 data[3][2] = 1;
 data[3][3] = 1;
+data[3][4] = 0.5;
 
 (() => {
   const c = document.getElementById("c");
@@ -26,7 +28,7 @@ data[3][3] = 1;
 
   drawData(ctx);
 
-  // drawOutlineCulledSmooth(ctx);
+  drawSurface(ctx);
 })();
 
 function drawBackground(canvas, ctx) {
@@ -65,6 +67,53 @@ function drawData(ctx) {
   }
 }
 
+const surroundingSamples = [[0, 0], [1, 0], [0, 1], [1, 1]];
+
+function drawSurface(ctx) {
+  for (let y = 0; y < grids; y++) {
+    for (let x = 0; x < grids; x++) {
+      const surroundings = [
+        data[x][y],
+        data[x + 1][y],
+        data[x][y + 1],
+        data[x + 1][y + 1]
+      ];
+      const surroundingsMass = surroundings.reduce(
+        (curr, prev) => curr + prev,
+        0
+      );
+      
+      if (surroundingsMass === 0) continue;
+
+      const centerOfMass = [
+        (-1 * surroundings[0] +
+          1 * surroundings[1] +
+          -1 * surroundings[2] +
+          1 * surroundings[3]) /
+          surroundingsMass,
+        (-1 * surroundings[0] +
+          -1 * surroundings[1] +
+          1 * surroundings[2] +
+          1 * surroundings[3]) /
+          surroundingsMass
+      ];
+
+      ctx.strokeStyle = "red";
+      strokeCircle(
+        ctx,
+        x * gridSize + gridSize / 2 + gridSize / 2 * centerOfMass[0],
+        y * gridSize + gridSize / 2 + gridSize / 2 * centerOfMass[1],
+        6
+      );
+    }
+  }
+
+  // ctx.beginPath();
+  // ctx.closePath();
+
+  // ctx.stroke();
+}
+
 function fillCircle(ctx, x, y, radius) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, Math.PI * 2);
@@ -72,95 +121,9 @@ function fillCircle(ctx, x, y, radius) {
   ctx.fill();
 }
 
-const surroundingSamples = [
-  [0, 0],
-  [1, 0],
-  [0, 1],
-  [1, 1],
-];
-
-function drawOutlineCulledSmooth(ctx) {
-  ctx.strokeStyle = "red";
-
-  // ctx.beginPath();
-
-  for (let y = 0; y < grids; y++) {
-    for (let x = 0; x < grids; x++) {
-      if (data[x][y] === 0) continue;
-
-      const isTop = y === 0;
-      const isBottom = y === data.length - 1;
-      const isLeft = x === 0;
-      const isRight = x === data.length - 1;
-      const surroundings = [
-        !isLeft && !isTop ? data[x - 1][y - 1] : 0,
-        !isTop ? data[x][y - 1] : 0,
-        !isRight && !isTop ? data[x + 1][y - 1] : 0,
-        !isLeft ? data[x - 1][y] : 0,
-        data[x][y],
-        !isRight ? data[x + 1][y] : 0,
-        !isLeft && !isBottom ? data[x - 1][y + 1] : 0,
-        !isBottom ? data[x][y + 1] : 0,
-        !isRight && !isBottom ? data[x + 1][y + 1] : 0
-      ];
-      const surroundingsMass = surroundings.reduce(
-        (curr, prev) => curr + prev,
-        0
-      );
-
-      const centerOfMass = [
-        (-1 * surroundings[0] +
-          0 * surroundings[1] +
-          1 * surroundings[2] +
-          -1 * surroundings[3] +
-          0 * surroundings[4] +
-          1 * surroundings[5] +
-          -1 * surroundings[6] +
-          0 * surroundings[7] +
-          1 * surroundings[8]) /
-          surroundingsMass,
-        (-1 * surroundings[0] +
-          -1 * surroundings[1] +
-          -1 * surroundings[2] +
-          0 * surroundings[3] +
-          0 * surroundings[4] +
-          0 * surroundings[5] +
-          1 * surroundings[6] +
-          1 * surroundings[7] +
-          1 * surroundings[8]) /
-          surroundingsMass
-      ];
-
-      console.log(centerOfMass);
-
-      ctx.fillStyle = "red";
-      fillCircle(
-        ctx,
-        x * gridSize + gridSize / 2 + gridSize * centerOfMass[0],
-        y * gridSize + gridSize / 2 + gridSize * centerOfMass[1],
-        10
-      );
-
-      // if (y > 0 && data[x][y - 1] === 0) {
-      //   ctx.moveTo(gridSize * data[x][y], gridSize * data[x][y]);
-      //   ctx.lineTo(gridSize, 0);
-      // }
-      // if (x < chunkCount - 1 && data[x + 1][y] === 0) {
-      //   ctx.moveTo(gridSize, 0);
-      //   ctx.lineTo(gridSize, gridSize);
-      // }
-      // if (y < chunkCount - 1 && data[x][y + 1] === 0) {
-      //   ctx.moveTo(gridSize, gridSize);
-      //   ctx.lineTo(0, gridSize);
-      // }
-      // if (x > 0 && data[x - 1][y] === 0) {
-      //   ctx.moveTo(0, gridSize);
-      //   ctx.lineTo(0, 0);
-      // }
-      // ctx.resetTransform();
-    }
-  }
-  // ctx.closePath();
-
-  // ctx.stroke();
+function strokeCircle(ctx, x, y, radius) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.stroke();
 }
