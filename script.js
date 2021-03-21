53c2a0f012409337505c14d74f193aaae92b742a
Glitch (gigantic-mighty-woodwind) <none>
2021-03-21 16:20:40 +0000

    🐴⛵️ Checkpoint ./script.js:5313652/22686

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const worldSize = 300;
-const gridSize = 50;
-const grids = worldSize / gridSize;
-const samples = grids + 1;
+// [0, 1, 0, 2, 1, 3, 2, 3];
+// prettier-ignore
+const squareEdges = [
+  [[0, 0], [1, 0]],
+  [[0, 0], [0, 1]],
+  [[1, 0], [1, 1]],
+  [[0, 1], [1, 1]],
+];
 
-(() => {
+const worldSize = 300;
+const grids = 6;
+const samples = grids + 1;
+const gridSize = worldSize / grids;
+
+function main() {
   const c = document.getElementById("c");
   c.style.width = `${worldSize}px`;
   c.style.height = `${worldSize}px`;
@@ -30,30 +39,21 @@ const samples = grids + 1;
   data[4][3] = 1;
   data[4][4] = 1;
 
-  drawData(ctx, data);
+  // drawData(ctx, data);
+
+  const data2 = new Data(samples, samples);
+  data2.set(2, 3, 1);
+  data2.set(3, 2, 1);
+  data2.set(3, 3, 1);
+  data2.set(3, 4, 1);
+  data2.set(4, 3, 1);
+  data2.set(4, 4, 1);
+  drawData2(ctx, data2);
 
   // const vertices = getVertices(data);
   // drawLines(vertices);
 
   drawSurface(ctx, data);
-})();
-
-function drawBackground(canvas, ctx) {
-  ctx.fillStyle = "#fff";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  ctx.strokeStyle = "#0004";
-  ctx.beginPath();
-  for (let i = 0; i <= ctx.canvas.width; i += gridSize) {
-    ctx.moveTo(i, 0);
-    ctx.lineTo(i, ctx.canvas.height);
-  }
-  for (let i = 0; i <= ctx.canvas.height; i += gridSize) {
-    ctx.moveTo(0, i);
-    ctx.lineTo(ctx.canvas.width, i);
-  }
-  ctx.closePath();
-  ctx.stroke();
 }
 
 function drawData(ctx, data) {
@@ -86,7 +86,6 @@ function drawSurface(ctx, data) {
       ctx.fillStyle = "#00f4";
       fillCircle(ctx, (x + 0.5) * gridSize, (y + 0.5) * gridSize, 2);
 
-      const crossings2 = [];
       const crossings = [];
       const edges = [
         [[0, 0], [1, 0]],
@@ -140,6 +139,24 @@ function drawSurface(ctx, data) {
   // ctx.stroke();
 }
 
+function drawBackground(canvas, ctx) {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.strokeStyle = "#0004";
+  ctx.beginPath();
+  for (let i = 0; i <= ctx.canvas.width; i += gridSize) {
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, ctx.canvas.height);
+  }
+  for (let i = 0; i <= ctx.canvas.height; i += gridSize) {
+    ctx.moveTo(0, i);
+    ctx.lineTo(ctx.canvas.width, i);
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
+
 function fillCircle(ctx, x, y, radius) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, Math.PI * 2);
@@ -177,3 +194,59 @@ function setX(v, x) {
 function setY(v, y) {
   return (v[1] = y);
 }
+
+class Data {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.buffer = Array.from({ length: width * height }).fill(0);
+  }
+
+  set(x, y, value) {
+    this.buffer[this.bufferIndex(x, y)] = value;
+  }
+
+  get(x, y) {
+    return this.buffer[this.bufferIndex(x, y)];
+  }
+
+  bufferIndex(x, y) {
+    return y * this.height + x;
+  }
+}
+
+function drawData2(ctx, data) {
+  for (let y = 0; y < data.height; y++) {
+    for (let x = 0; x < data.width; x++) {
+      if (data.get(x, y) === 0) continue;
+      ctx.fillStyle = "#f008";
+      fillCircle(
+        ctx,
+        x * gridSize,
+        y * gridSize,
+        (gridSize / 2) * data.get(x, y)
+      );
+    }
+  }
+}
+
+function drawSurface2(ctx, data) {
+  for (let y = 0; y < data.height - 1; y++) {
+    for (let x = 0; x < data.width - 1; x++) {
+      let corners = 0b0000;
+      for (let j = 0; j < 2; j++) {
+        for (let i = 0; i < 2; i++) {
+          const p = data.get(x + i, y + j);
+          corners = corners || 0 < p ? 1 << (j * 2 + i) : 0;
+        }
+      }
+      if (corners === 0b0000 || corners === 0b1111) continue;
+
+      for (let i = 0; i < 4; i++) {
+        data.get();
+      }
+    }
+  }
+}
+
+main();
