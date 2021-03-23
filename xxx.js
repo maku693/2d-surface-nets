0dd60d9379f563bdcb9e6159985e8549c09a7ab4
Glitch (gigantic-mighty-woodwind) <none>
2021-03-23 16:48:52 +0000

    🌲🏝 Checkpoint ./xxx.js:5313652/86

--- xxx.js
+++ xxx.js
@@ -1,20 +1,20 @@
 var mask = 0b1000 // 0
 var edge_table = [
   0b0000, // i: 0b0000
-  0b0000, // i: 0b0001
-  0b0000, // i: 0b0010
-  0b0000, // i: 0b0011
-  0b0000, // i: 0b0100
-  0b0000, // i: 0b0101
-  0b0000, // i: 0b0110
-  0b0000, // i: 0b0111
+  0b1001, // i: 0b0001
+  0b0011, // i: 0b0010
+  0b1011, // i: 0b0011
+  0b0110, // i: 0b0100
+  0b1111, // i: 0b0101
+  0b0111, // i: 0b0110
+  0b1111, // i: 0b0111
   0b1100, // i: 0b1000
 ];
 
-var edge_mask = edge_table[mask]; // 0b1000
+var edge_mask = edge_table[mask]; // 0b1100
 
 var i = 2;
-var crossed = edge_mask & (1 << i); // edge_mask & 0b0100
+var crossed = edge_mask & (1 << i); // 0b1100 & 0b0100
 
 var i = 3;
-var crossed = edge_mask & (1 << i); // edge_mask & 0b1000
+var crossed = edge_mask & (1 << i); // 0b1100 & 0b1000
