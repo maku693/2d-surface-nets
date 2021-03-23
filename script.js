0ef1c05b4c28a7ed03667018107a36130f99dd21
Glitch (gigantic-mighty-woodwind) <none>
2021-03-23 12:48:32 +0000

    ⏲❄️ Checkpoint ./script.js:5313652/5340

--- script.js
+++ script.js
@@ -28,24 +28,6 @@ function main() {
 
   drawBackground(c, ctx);
 
-  const data = Array.from({ length: samples }, () =>
-    new Array(samples).fill(0)
-  );
-
-  data[2][3] = 1;
-  data[3][2] = 1;
-  data[3][3] = 1;
-  data[3][4] = 1;
-  data[4][3] = 1;
-  data[4][4] = 1;
-
-  // drawData(ctx, data);
-
-  // const vertices = getVertices(data);
-  // drawLines(vertices);
-
-  // drawSurface(ctx, data);
-
   const data2 = new Data(samples, samples);
   data2.set(2, 3, 1);
   data2.set(3, 2, 1);
@@ -55,89 +37,7 @@ function main() {
   data2.set(4, 4, 1);
   
   drawData2(ctx, data2);
-}
-
-function drawData(ctx, data) {
-  for (let y = 0; y < samples; y++) {
-    for (let x = 0; x < samples; x++) {
-      if (data[x][y] === 0) continue;
-      ctx.fillStyle = "#f008";
-      fillCircle(ctx, x * gridSize, y * gridSize, (gridSize / 2) * data[x][y]);
-    }
-  }
-}
-
-function drawSurface(ctx, data) {
-  const vertices = [];
-  const lines = [];
-  for (let y = 0; y < grids; y++) {
-    for (let x = 0; x < grids; x++) {
-      const surroundings = [
-        data[x][y],
-        data[x + 1][y],
-        data[x][y + 1],
-        data[x + 1][y + 1]
-      ];
-
-      if (!(surroundings.some(x => x === 0) && surroundings.some(x => 0 < x)))
-        continue;
-
-      ctx.fillStyle = "#00f2";
-      ctx.fillRect(x * gridSize, y * gridSize, gridSize, gridSize);
-      ctx.fillStyle = "#00f4";
-      fillCircle(ctx, (x + 0.5) * gridSize, (y + 0.5) * gridSize, 2);
-
-      const crossings = [];
-      const edges = [
-        [[0, 0], [1, 0]],
-        [[0, 0], [0, 1]],
-        [[1, 0], [1, 1]],
-        [[0, 1], [1, 1]]
-      ];
-      for (let i = 0; i < edges.length; i++) {
-        const edge = edges[i];
-        const [e1x, e1y] = [x + getX(edge[0]), y + getY(edge[0])];
-        const [e2x, e2y] = [x + getX(edge[1]), y + getY(edge[1])];
-        const e1 = data[e1x][e1y];
-        const e2 = data[e2x][e2y];
-        if (!((e1 === 0 && 0 < e2) || (0 < e1 && e2 === 0))) continue;
-
-        ctx.fillStyle = "#0802";
-        fillCircle(ctx, e1x * gridSize, e1y * gridSize, 2);
-        fillCircle(ctx, e2x * gridSize, e2y * gridSize, 2);
-        fillCircle(
-          ctx,
-          (e1x + e2x) * 0.5 * gridSize,
-          (e1y + e2y) * 0.5 * gridSize,
-          2
-        );
-
-        // ctx.fillStyle = "#0802";
-        // fillCircle(ctx, e1x * gridSize, e1y * gridSize, 2);
-
-        crossings.push([(e1x + e2x) / 2, (e1y + e2y) / 2]);
-      }
-
-      const vertex = [
-        (crossings[0][0] + crossings[1][0]) * 0.5,
-        (crossings[0][1] + crossings[1][1]) * 0.5
-      ];
-
-      ctx.fillStyle = "#00f8";
-      fillCircle(ctx, vertex[0] * gridSize, vertex[1] * gridSize, 3);
-
-      vertices.push(vertex);
-
-      lines.push([]);
-    }
-  }
-
-  // TODO: stroke lines
-
-  // ctx.beginPath();
-  // ctx.closePath();
-
-  // ctx.stroke();
+  drawSurface2(ctx, data2);
 }
 
 function drawBackground(canvas, ctx) {
@@ -251,3 +151,104 @@ function drawSurface2(ctx, data) {
 }
 
 main();
+
+//   const data = Array.from({ length: samples }, () =>
+//     new Array(samples).fill(0)
+//   );
+
+//   data[2][3] = 1;
+//   data[3][2] = 1;
+//   data[3][3] = 1;
+//   data[3][4] = 1;
+//   data[4][3] = 1;
+//   data[4][4] = 1;
+
+//   drawData(ctx, data);
+
+//   const vertices = getVertices(data);
+//   drawLines(vertices);
+
+//   drawSurface(ctx, data);
+
+function drawData(ctx, data) {
+  for (let y = 0; y < samples; y++) {
+    for (let x = 0; x < samples; x++) {
+      if (data[x][y] === 0) continue;
+      ctx.fillStyle = "#f008";
+      fillCircle(ctx, x * gridSize, y * gridSize, (gridSize / 2) * data[x][y]);
+    }
+  }
+}
+
+function drawSurface(ctx, data) {
+  const vertices = [];
+  const lines = [];
+  for (let y = 0; y < grids; y++) {
+    for (let x = 0; x < grids; x++) {
+      const surroundings = [
+        data[x][y],
+        data[x + 1][y],
+        data[x][y + 1],
+        data[x + 1][y + 1]
+      ];
+
+      if (!(surroundings.some(x => x === 0) && surroundings.some(x => 0 < x)))
+        continue;
+
+      ctx.fillStyle = "#00f2";
+      ctx.fillRect(x * gridSize, y * gridSize, gridSize, gridSize);
+      ctx.fillStyle = "#00f4";
+      fillCircle(ctx, (x + 0.5) * gridSize, (y + 0.5) * gridSize, 2);
+
+      const crossings = [];
+      const edges = [
+        [[0, 0], [1, 0]],
+        [[0, 0], [0, 1]],
+        [[1, 0], [1, 1]],
+        [[0, 1], [1, 1]]
+      ];
+      for (let i = 0; i < edges.length; i++) {
+        const edge = edges[i];
+        const [e1x, e1y] = [x + getX(edge[0]), y + getY(edge[0])];
+        const [e2x, e2y] = [x + getX(edge[1]), y + getY(edge[1])];
+        const e1 = data[e1x][e1y];
+        const e2 = data[e2x][e2y];
+        if (!((e1 === 0 && 0 < e2) || (0 < e1 && e2 === 0))) continue;
+
+        ctx.fillStyle = "#0802";
+        fillCircle(ctx, e1x * gridSize, e1y * gridSize, 2);
+        fillCircle(ctx, e2x * gridSize, e2y * gridSize, 2);
+        fillCircle(
+          ctx,
+          (e1x + e2x) * 0.5 * gridSize,
+          (e1y + e2y) * 0.5 * gridSize,
+          2
+        );
+
+        // ctx.fillStyle = "#0802";
+        // fillCircle(ctx, e1x * gridSize, e1y * gridSize, 2);
+
+        crossings.push([(e1x + e2x) / 2, (e1y + e2y) / 2]);
+      }
+
+      const vertex = [
+        (crossings[0][0] + crossings[1][0]) * 0.5,
+        (crossings[0][1] + crossings[1][1]) * 0.5
+      ];
+
+      ctx.fillStyle = "#00f8";
+      fillCircle(ctx, vertex[0] * gridSize, vertex[1] * gridSize, 3);
+
+      vertices.push(vertex);
+
+      lines.push([]);
+    }
+  }
+
+  // TODO: stroke lines
+
+  // ctx.beginPath();
+  // ctx.closePath();
+
+  // ctx.stroke();
+}
