0bfc1b258c5cc622fc53f17692d8d65d558f9609
maku693 <me@maku693.net>
2021-04-05 22:42:12 +0900

    Checkpoint

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const worldSize = 300;
-const grids = 5;
+const samplePoints = 6;
+const grids = samplePoints - 1;
 const gridSize = worldSize / grids;
-const samplePoints = grids + 1;
+
+const squareEdges = [0, 1, 0, 2, 1, 3, 2, 3];
+
+/**
+ * cornersToEdges is edges indexed by corners.
+ * edges and corners are binary flag bits.
+ * a bit in corners represents which corners are in the volume.
+ * a bit in edges represents which edges are adjacent to the corner.
+ */
+const cornersToEdges = new Uint8Array(1 << 4);
+{
+  for (let i = 0; i < cornersToEdges.length; i++) {
+    let edges = 0;
+    for (let j = 0; j < 4; j++) {
+      const a = !!(i & (1 << squareEdges[j * 2]));
+      const b = !!(i & (1 << squareEdges[j * 2 + 1]));
+      edges |= a === b ? 0 : 1 << j;
+    }
+    cornersToEdges[i] = edges;
+  }
+}
 
 const data = new Uint8Array(grids * grids);
 function data_set(x, y, value) {
@@ -28,32 +49,43 @@ ctx.scale(scale, scale);
 ctx.lineWidth = 2;
 
 // background
-{
-  ctx.fillStyle = "#fff";
-  ctx.fillRect(0, 0, worldSize, worldSize);
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, worldSize, worldSize);
 
-  ctx.strokeStyle = "#0004";
-  ctx.beginPath();
-  for (let i = 0; i <= ctx.canvas.width; i += gridSize) {
-    ctx.moveTo(i, 0);
-    ctx.lineTo(i, ctx.canvas.height);
-  }
-  for (let i = 0; i <= ctx.canvas.height; i += gridSize) {
-    ctx.moveTo(0, i);
-    ctx.lineTo(ctx.canvas.width, i);
-  }
-  ctx.closePath();
-  ctx.stroke();
+ctx.beginPath();
+for (let i = 0; i <= ctx.canvas.width; i += gridSize) {
+  ctx.moveTo(i, 0);
+  ctx.lineTo(i, ctx.canvas.height);
 }
+for (let i = 0; i <= ctx.canvas.height; i += gridSize) {
+  ctx.moveTo(0, i);
+  ctx.lineTo(ctx.canvas.width, i);
+}
+ctx.closePath();
+ctx.strokeStyle = "#0002";
+ctx.stroke();
 
-// data
-{
-  for (let i = 0; i < data.length; i++) {
-    const p = data[i];
-    const x = (i % grids) * gridSize;
-    const y = parseInt(i / grids) * gridSize;
+ctx.beginPath();
+for (let i = gridSize / 2; i <= ctx.canvas.width; i += gridSize) {
+  ctx.moveTo(i, 0);
+  ctx.lineTo(i, ctx.canvas.height);
+}
+for (let i = gridSize / 2; i <= ctx.canvas.height; i += gridSize) {
+  ctx.moveTo(0, i);
+  ctx.lineTo(ctx.canvas.width, i);
+}
+ctx.closePath();
+ctx.setLineDash([5, 5]);
+ctx.stroke();
+ctx.setLineDash([]);
 
-    ctx.fillStyle = `#f00${(p * 0x8).toString(16)}`;
-    ctx.fillRect(x, y, gridSize, gridSize);
-  }
+for (let i = 0; i < data.length; i++) {
+  const p = data[i];
+  if (p === 0) continue;
+
+  const x = (i % grids) * gridSize;
+  const y = parseInt(i / grids) * gridSize;
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(x + gridSize - 4, y + gridSize - 4, 8, 8);
 }
